45d0bd5351000cb0eae418daf2928af607d58342
Mohammed <dev.mohammedsh@gmail.com>
2025-03-16 16:29:38 +0530

    🧹 Set up Prettier and format codebase ✨ - Add Prettier config 📝 - Run bun format for clean code 🖌️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
