ba816d86554311b7245a3e6746f633cce1479f2a
Mohammed <dev.mohammedsh@gmail.com>
2025-03-16 16:58:55 +0530

    📂 components migrations (Header, CD) 🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+// integrations
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
